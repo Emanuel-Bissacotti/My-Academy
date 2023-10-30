9c5f70b803e2d500a9826cc34f74f41a11c028c9
Emanuel-Bissacotti <emanuelfbissacotti@gmail.com>
2023-10-30 17:23:57 -0300

    Cria corpo da home page

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,13 +2,14 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
-  final User user;
-  const HomeScreen({required this.user, super.key});
+  final User? user;
+  const HomeScreen({this.user, super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.pink,
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(),
     );
   }
 }
