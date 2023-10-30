c965e939fad26d5c95a33705e550eefe76a37810
Emanuel-Bissacotti <emanuelfbissacotti@gmail.com>
2023-10-30 17:21:57 -0300

    Adiciona novas possiveis passagem por parametro
    Adiciona nova passagem por parametro para o componente
    do textFormField agora é possivel passar o validator e o tipo do
    teclado.

--- lib/components/build_text_field.dart
+++ lib/components/build_text_field.dart
@@ -3,18 +3,22 @@ import 'package:flutter/material.dart';
 class BuildFormField extends StatefulWidget {
   final String labelText;
   final bool showVisibilityIcon;
+  String? Function(String?)? validator;
+  TextInputType? keyboard;
   final TextEditingController controller;
   bool obscureText;
 
-  BuildFormField({
-    Key? key,
-    this.obscureText = false,
-    required this.labelText,
-    this.showVisibilityIcon = false,
-    required this.controller,
-  }) : super(key: key);
+  BuildFormField(
+      {Key? key,
+      this.obscureText = false,
+      required this.labelText,
+      this.showVisibilityIcon = false,
+      required this.controller,
+      this.keyboard})
+      : super(key: key);
 
   @override
+  // ignore: library_private_types_in_public_api
   _BuildFormFieldState createState() => _BuildFormFieldState();
 }
 
@@ -27,11 +31,19 @@ class _BuildFormFieldState extends State<BuildFormField> {
 
   @override
   Widget build(BuildContext context) {
+    defaultValidator(value) {
+      if (value == null || value.isEmpty) {
+        return 'Informe o ${widget.labelText} corretamente';
+      }
+      return null;
+    }
+
     return Padding(
       padding: const EdgeInsets.only(left: 40, bottom: 20, right: 40),
       child: TextFormField(
         controller: widget.controller, // Usar o TextEditingController fornecido
         obscureText: widget.obscureText,
+        keyboardType: widget.keyboard ?? TextInputType.text,
         decoration: InputDecoration(
           border: const OutlineInputBorder(),
           filled: true,
@@ -52,6 +64,7 @@ class _BuildFormFieldState extends State<BuildFormField> {
                 )
               : null,
         ),
+        validator: widget.validator ?? defaultValidator,
       ),
     );
   }
